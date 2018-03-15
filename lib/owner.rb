ceedfa08cea40be6dc4b375025ff994c6d0930d7
Mark Cruz <markchriscruz@yahoo.com>
2018-03-15 15:50:48 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,41 @@
 class Owner
-  # code goes here
+  attr_reader :species, :pets
+  attr_accessor :name
+  @@all = []
+  @@count = 0
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @@count += 1
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+  
+  def self.count
+    return @@count
+  end
+  
+  def self.all
+    return @@all
+  end
+  
+  def self.reset_all
+    @@all.clear
+    @@count = 0
+  end
+  
+  def say_species
+    return "I am a #{@species}."
+  end
+  
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+  
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
 end
